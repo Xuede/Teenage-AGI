d20b77082a3814e06cc9b0df569395b33b5070c4
Xuede <mdub@i3ex.org>
2023-08-03 14:32:15 -0400

    init commit
    init commit

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
-import agent
 import os
+import datetime
 from agent import Agent
 from dotenv import load_dotenv
+
+def load_initial_time():
+    print("In load initial time")
+    TIME_FILE = 'last_interaction.txt'
+    try:
+        with open(TIME_FILE) as f:
+            time_str = f.read()
+            print("Time str:", time_str)
+    except:
+        print("Error reading time file")
+        return datetime.datetime.now()
+
+    if not time_str:  
+        time_str = datetime.datetime.utcnow().isoformat()
+
+    return datetime.datetime.fromisoformat(time_str)
 
 # Load default environment variables (.env)
 load_dotenv()
 
 AGENT_NAME = os.getenv("AGENT_NAME", "my-agent")
-
-agent = Agent(AGENT_NAME)
+TIME_FILE = 'TIME_FILE.txt'
+initial_time = load_initial_time()
+table_name = "my-agent"
+#name = "my-agent"
+agent = Agent(initial_time=initial_time, table_name=table_name)
+agent.update_time()
 
 # Creates Pinecone Index
 agent.createIndex()
@@ -18,16 +38,22 @@ print("Talk to the AI!")
 while True:
     userInput = input()
     if userInput:
-        if (userInput.startswith("read:")):
+        if userInput.startswith("read:"):
             agent.read(" ".join(userInput.split(" ")[1:]))
             print("Understood! The information is stored in my memory.")
-        elif (userInput.startswith("think:")):
+        elif userInput.startswith("think:"):
             agent.think(" ".join(userInput.split(" ")[1:]))
             print("Understood! I stored that thought into my memory.")
-        elif (userInput.startswith("readDoc:")):
+        elif userInput.startswith("readDoc:"):
             agent.readDoc(" ".join(userInput.split(" ")[1:]))
             print("Understood! I stored the document into my memory.")
         else:
-            print(agent.action(userInput), "\n")
+            response = agent.action(userInput)
+            print(response)
+            # Save latest time after interacting
+            
+            with open(TIME_FILE, 'r') as f:
+                datetime_str = f.read().strip()  # read from file and remove any leading/trailing whitespace
+
     else:
         print("SYSTEM - Give a valid input")
