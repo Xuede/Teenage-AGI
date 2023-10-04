7b940dd53015a25fdb5cc1f64b9dedd7d776e32d
xuede <mdub@i3ex.org>
2023-10-04 12:08:00 -0400

    Update keep_alive.py

--- .github/workflows/keep_alive.py
+++ .github/workflows/keep_alive.py
@@ -1,6 +1,10 @@
 import os
 import pinecone
 
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+PINECONE_API_ENV = os.getenv("PINECONE_API_ENV")
+
 pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_API_ENV)
 index_name = 'my-agent'
 
