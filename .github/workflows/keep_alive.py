37a4062cbb9fe8be28745339b6f17f255272bdb5
xuede <mdub@i3ex.org>
2023-10-04 21:07:13 -0400

    Update keep_alive.py

--- .github/workflows/keep_alive.py
+++ .github/workflows/keep_alive.py
@@ -1,15 +1,15 @@
 import os
-import pinecone
+from pinecone import Client
 
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 PINECONE_API_ENV = os.getenv("PINECONE_API_ENV")
 index_name = 'my-agent'
 
 try:
-    client = pinecone.Client(api_key=PINECONE_API_KEY, environment=PINECONE_API_ENV)
+    client = Client(api_key=PINECONE_API_KEY, environment=PINECONE_API_ENV)
 
     # Fetch some info to keep the index alive
-    info = pinecone.info(index_name)
+    info = client.info(index_name)
     print(f"Index info: {info}")
 except Exception as e:
     print(f"An error occurred: {str(e)}")
