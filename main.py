e025932fd5a63cc48440c7b1592b5feaecb53a5c
xuede <mdub@i3ex.org>
2023-10-26 23:46:10 -0400

    updated memcount

--- main.py
+++ main.py
@@ -3,21 +3,55 @@ import datetime
 from agent import Agent
 from dotenv import load_dotenv
 
-def load_initial_time():
-    print("In load initial time")
-    TIME_FILE = 'last_interaction.txt'
-    try:
-        with open(TIME_FILE) as f:
-            time_str = f.read()
-            print("Time str:", time_str)
-    except:
-        print("Error reading time file")
-        return datetime.datetime.now()
+# Define the last interaction time
+last_interaction_time = datetime.datetime.now()
+agent.think(f"The current date and time is {last_interaction_time.strftime('%Y-%m-%d %H:%M:%S')}")
 
-    if not time_str:  
-        time_str = datetime.datetime.utcnow().isoformat()
+print(f"Hello, I'm Xan. How's tricks?")
 
-    return datetime.datetime.fromisoformat(time_str)
+input_received = False
+stop_timer_thread = False
+
+def read_time_periodically():
+    global stop_timer_thread
+    while not stop_timer_thread:
+        #print("Time update function is called.")
+        agent.update_time()
+        #print("Time update function has completed. Sleeping for 5 minutes now.")
+        for _ in range(300):  # sleeps for 300 seconds (5 minutes)
+            if not stop_timer_thread:
+                time.sleep(1)
+            else:
+                break
+
+def check_for_input():
+    global input_received, stop_timer_thread
+    while True:
+        userInput = input('Input to AI: ')
+        if userInput.lower() == "farewell":
+            response = agent.action(userInput)
+            print(response, "\n")
+            stop_timer_thread = True  # stop the timer thread
+            time.sleep(5)  # wait for 5 seconds to allow for graceful exit
+            exit(0)  # gracefully terminate the program
+        elif userInput.lower().startswith("read:"):
+            agent.read(" ".join(userInput.split(" ")[1:]))
+        elif userInput.lower().startswith("think:"):
+            agent.think(" ".join(userInput.split(" ")[1:]))
+        else:
+            agent.update_time()  # Update time before responding
+            response = agent.action(userInput)
+            print(response, "\n")
+
+# Create and start the threads
+timer_thread = threading.Thread(target=read_time_periodically)
+input_thread = threading.Thread(target=check_for_input)
+timer_thread.start()
+input_thread.start()
+
+# Wait for both threads to finish
+timer_thread.join()
+input_thread.join()
 
 # Load default environment variables (.env)
 load_dotenv()
